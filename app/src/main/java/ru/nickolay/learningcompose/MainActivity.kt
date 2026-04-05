f4faee7658cc6fc0f5a1ccb457f3ee936ecebbf1
Nickolay <piunnk@yandex.ru>
2026-04-05 18:13:59 +0300

    viewModel add

--- app/src/main/java/ru/nickolay/learningcompose/MainActivity.kt
+++ app/src/main/java/ru/nickolay/learningcompose/MainActivity.kt
@@ -1,16 +1,21 @@
 package ru.nickolay.learningcompose
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
@@ -25,127 +30,37 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import ru.nickolay.learningcompose.navigation.vkNewsClient
+import ru.nickolay.learningcompose.ui.theme.LearningComposeTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         enableEdgeToEdge()
         setContent {
-
+            LearningComposeTheme() {
+                InstagramProfileCard(viewModel)
+            }
         }
     }
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Test() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                ),
-                title = { Text("TopAppBar") },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(painter = painterResource(R.drawable.ic_menu), contentDescription = null)
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {}) {
-                        Icon(painterResource(R.drawable.ic_like), contentDescription = null)
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                actions = {
-                    IconButton(onClick = {}) {Icon(painterResource(R.drawable.ic_like), contentDescription = null) }
-                    IconButton(onClick = {}) {Icon(painterResource(R.drawable.ic_comment), contentDescription = null) }
-                    IconButton(onClick = {}) {Icon(painterResource(R.drawable.ic_share), contentDescription = null) }
-                },
-                floatingActionButton = {
-                    FloatingActionButton(
-                        onClick = {},
-                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-                    ) {
-                        Icon(painterResource(R.drawable.ic_launcher_foreground), "Localize description")
-                    }
-                }
-            )
-        },
-
-    ) {
-        Text(
-            modifier = Modifier.padding(it),
-            text = "This is scaffold test"
-        )
-    }
-}
 
 
 
-//@Composable
-//fun Example1() {
-//    OutlinedButton(onClick = {}) {
-//        Text(text = "Hello World")
-//    }
-//}
-//
-//@Composable
-//fun Example2() {
-//    TextField(
-//        value = "Value",
-//        onValueChange = {},
-//        label = { Text(text = "Label") }
-//    )
-//}
-//
-//@Composable
-//fun Example3() {
-//    AlertDialog(
-//        onDismissRequest = {},
-//        confirmButton = {
-//            Text(
-//                modifier = Modifier.padding(8.dp),
-//                text = "Yes"
-//            )
-//        },
-//        title = {
-//            Text(
-//                text = "Are you sure?"
-//            )
-//        },
-//        text = {
-//            Text(
-//                text = "Do you want to delete this file?"
-//            )
-//        },
-//        dismissButton = {
-//            Text(
-//                modifier = Modifier.padding(8.dp),
-//                text = "No"
-//            )
-//        }
-//    )
-//}
 
-@Preview
-@Composable
-fun PreviewCheckBox() {
-    Test()
-}
 
 
 
---
+++ app/src/main/java/ru/nickolay/learningcompose/navigation/NavigationItems.kt
@@ -0,0 +1,15 @@
+package ru.nickolay.learningcompose.navigation
+
+
+import android.media.Image
+import androidx.compose.ui.res.painterResource
+import ru.nickolay.learningcompose.R
+
+sealed class NavigationItems(
+    val titleResId: Int,
+    val image: Int
+) {
+    object Home: NavigationItems(R.string.navigation_item_main,  R.drawable.ic_home)
+    object Favorite: NavigationItems(R.string.navigation_item_favorite, image = R.drawable.ic_like_selected)
+    object Profile: NavigationItems(R.string.navigation_item_profile, R.drawable.ic_profile)
+}
